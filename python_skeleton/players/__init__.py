cc4fafdc1d7822855fe0201f95a1f1cb76ce9e7a
vihank <17678458+vihank@users.noreply.github.com>
2024-03-29 23:07:12 -0400

    organization

---
+++ python_skeleton/players/__init__.py
@@ -0,0 +1,3 @@
+from all_in import Player as AllInPlayer
+from prob_bot import Player as ProbBotPlayer
+from arnav_prob import Player as ArnavProbPlayer
--- python_skeleton/players/arnav_prob.py
+++ python_skeleton/players/arnav_prob.py
@@ -3,13 +3,8 @@ Simple example pokerbot, written in Python.
 """
 
 import itertools
-import random
 import pickle
-import math
-import numpy as np
 from typing import Optional
-
-from players import *
 
 from skeleton.actions import Action, CallAction, CheckAction, FoldAction, RaiseAction
 from skeleton.states import GameState, TerminalState, RoundState
@@ -18,10 +13,11 @@ from skeleton.bot import Bot
 from skeleton.runner import parse_args, run_bot
 from skeleton.evaluate import evaluate
 
-class Player(Bot):
+class ArnavPlayer(Bot):
     """
     A pokerbot.
     """
+
     def __init__(self) -> None:
         """
         Called when a new game starts. Called exactly once.
@@ -32,19 +28,9 @@ class Player(Bot):
         Returns:
         Nothing.
         """
-        
-        self.weighting = np.array([0.01, 0.5, 1.0])
-        self.c = 0.5
-        
-        self.bots = {0: AllInPlayer(), 1: ProbBotPlayer(), 2: ArnavProbPlayer()}
-        
-        self.cur_bot = 0
-        self.num_bots = len(self.bots)
-        
-        self.times_chosen = np.zeros(self.num_bots)
-        self.payoffs = np.zeros(self.num_bots)
-        self.num_turns = 0
-        print("initialized")
+        self.log = []
+        self.pre_computed_probs = pickle.load(open("python_skeleton/skeleton/pre_computed_probs.pkl", "rb")) 
+        pass
 
     def handle_new_round(self, game_state: GameState, round_state: RoundState, active: int) -> None:
         """
@@ -66,15 +52,7 @@ class Player(Bot):
         self.log = []
         self.log.append("================================")
         self.log.append("new round")
-        self.num_turns = 0
-
-        soft_rewards = np.exp(self.payoffs) / np.sum(np.exp(self.payoffs))
-        ucb = 1 + self.c * np.sqrt(np.log(self.num_turns) * np.reciprocal(self.times_chosen))
-        probs = soft_rewards * self.weighting * ucb
-        norm_probs = probs / np.sum(probs)
-        self.cur_bot = np.random.choice(list(range(self.num_bots)), p = norm_probs)
-
-        self.bots[self.cur_bot].handle_new_round(game_state, round_state, active)
+        pass
 
     def handle_round_over(self, game_state: GameState, terminal_state: TerminalState, active: int, is_match_over: bool) -> Optional[str]:
         """
@@ -95,10 +73,6 @@ class Player(Bot):
         #opp_cards = previous_state.hands[1-active] # opponent's cards or [] if not revealed
         self.log.append("game over")
         self.log.append("================================\n")
-        self.num_turns += 1
-        self.payoffs[self.cur_bot] = (self.payoffs[self.cur_bot] * self.times_chosen[self.cur_bot] + terminal_state.deltas[active]) / (self.times_chosen[self.cur_bot] + 1)
-        self.times_chosen[self.cur_bot] += 1
-        self.bots[self.cur_bot].handle_round_over(game_state, terminal_state, active, is_match_over)
 
         return self.log
 
@@ -126,10 +100,6 @@ class Player(Bot):
         Returns:
             Action: The action you want to take.
         """
-<<<<<<< HEAD
-        
-        return self.bots[self.cur_bot].get_action(observation)
-=======
         my_contribution = STARTING_STACK - observation["my_stack"] # the number of chips you have contributed to the pot
         opp_contribution = STARTING_STACK - observation["opp_stack"] # the number of chips your opponent has contributed to the pot
         pot_size = my_contribution + opp_contribution # the number of chips in the pot
@@ -157,39 +127,22 @@ class Player(Bot):
 
         # If the villain raised, adjust the probability
         if continue_cost > 1:
-            if observation["opp_stack"] == 0:
-                self.num_shoves += 1
-        if (self.num_shoves / self.num_rounds >= 0.2 and 
-            (random.random() >= 0.1)):
-            if equity > 0.51 and (RaiseAction in observation["legal_actions"]):
-                action = RaiseAction(observation["max_raise"])
-            elif equity > 0.51 and (CallAction in observation["legal_actions"]):
-                action = CallAction()
-            elif CheckAction in observation["legal_actions"]:
-                action = CheckAction()
-            else:
-                action = FoldAction()
+            equity = (equity - 0.5) / 0.5
+            self.log.append(f"Adjusted equity: {equity}")
+        if equity > 0.8 and RaiseAction in observation["legal_actions"]:
+            raise_amount = min(int(pot_size*0.75), observation["max_raise"])
+            raise_amount = max(raise_amount, observation["min_raise"])
+            action = RaiseAction(raise_amount)
+        elif CallAction in observation["legal_actions"] and equity >= pot_odds:
+            action = CallAction()
+        elif CheckAction in observation["legal_actions"]:
+            action = CheckAction()
         else:
-            if continue_cost > 1:
-                equity = (equity - 0.5) / 0.5
-                self.log.append(f"Adjusted equity: {equity}")
-            if equity > 0.9 and RaiseAction in observation["legal_actions"]:
-                action = RaiseAction(observation["max_raise"])
-            elif equity > 0.8 and RaiseAction in observation["legal_actions"]:
-                raise_amount = min(int(pot_size*0.75), observation["max_raise"])
-                raise_amount = max(raise_amount, observation["min_raise"])
-                action = RaiseAction(raise_amount)
-            elif CallAction in observation["legal_actions"] and equity >= pot_odds:
-                action = CallAction()
-            elif CheckAction in observation["legal_actions"]:
-                action = CheckAction()
-            else:
-                action = FoldAction()
+            action = FoldAction()
 
         self.log.append(str(action) + "\n")
 
         return action
->>>>>>> dd8a972a1188ceb55b7e9306aac012f3fb657805
 
 if __name__ == '__main__':
-    run_bot(Player(), parse_args())
+    run_bot(ArnavPlayer(), parse_args())
