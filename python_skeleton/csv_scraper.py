1ac9cbf832ca285ed0dbc128cb793b7c1159981c
notbamity <arnavp@andrew.cmu.edu>
2024-03-30 04:27:05 -0400

    csv scraper made

---
+++ python_skeleton/csv_scraper.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+class Game():
+    def __init__(self):
+        self.hands = []
+
+    def add_episode(self, info):
+        self.hands.append(info)
+
+    def __repr__(self):
+        return '\t'.join([x for x in self.hands])
+
+class Hand():
+    __allowed = ("isfirst", "reward", "hand", "opp_hand", "turns")
+    def __init__(self, **kwarg):
+        for k, v in kwarg.items():
+            assert(k in self.__class__.__allowed)
+            setattr(self, k, v)
+
+    def add_turns(self, turns):
+        self.turns = turns
+
+
+path = "C:/Users/Arnav/Downloads/engine_log (1).csv"
+df = pd.read_csv(path)
+teamname = "vihank"
+rounds = list()
+yourteam = 0
+games = Game()
+
+for i in range(1,5):
+    episode = dict()
+    temp = df.loc[df["Round"] == i,]
+    fplayer = temp.iloc[0,].loc["Team",]
+    if (i == 1 and fplayer == teamname):
+        yourteam = 1
+    else:
+        yourteam = 2
+    curr = df.loc[df["Round"] == i,]
+    next = df.loc[df["Round"] == i+1].iloc[0,]
+    isfirst = (fplayer == teamname)
+    reward = next.loc["Bankroll"] - curr.iloc[0,].loc["Bankroll"]
+
+    if (yourteam == 1):
+        hand = curr.iloc[0,].loc["Team1Cards"]
+        opp_hand = curr.iloc[0,].loc["Team2Cards"]
+    else:
+        hand = curr.iloc[0,].loc["Team2Cards"]
+        opp_hand = curr.iloc[0,].loc["Team1Cards"]
+
+    episode = Hand(isfirst=isfirst, reward=reward, hand=hand, opp_hand=opp_hand)
+    actions = []
+
+    for j in range(0,3,1):
+        turn = defaultdict(list)
+        temp = curr.loc[df["Street"] == j,] 
+        if temp.empty:
+            break
+        turn["street"] = j
+        plays = np.shape(temp)[0]
+        if (j == 0):
+            used = 2
+        else:
+            used = 0
+        left = plays - used
+        turn["public_cards"] = temp.iloc[0,].loc["AllCards"]
+        while (left > 1):
+            betting_round = temp.iloc[used:(used+2),]
+            you = betting_round.loc[df["Team"] == teamname,]
+            opp = betting_round.loc[-(df["Team"] == teamname),]
+            if "your_action" not in turn:
+                turn["your_action"].append(you.loc[:,"Action"].item())
+                turn["your_amount"].append(you.loc[:,"ActionAmt"].item())
+                turn["opp_action"].append(opp.loc[:,"Action"].item())
+                turn["opp_amount"].append(opp.loc[:,"ActionAmt"].item())
+            else:
+                turn["your_action"].append(you.loc[:,"Action"].item())
+                turn["your_amount"].append(you.loc[:,"ActionAmt"].item())
+                turn["opp_action"].append(opp.loc[:,"Action"].item())
+                turn["opp_amount"].append(opp.loc[:,"ActionAmt"].item())
+            used += 2
+            left = plays - used
+        if (left == 1 and (fplayer == teamname)):
+            betting_round = temp.iloc[-1,]
+            if "your_action" not in turn:
+                turn["your_action"].append(you.loc[:, "Action"].item())
+                turn["your_amount"].append(you.loc[:, "ActionAmt"].item())
+                turn["opp_action"].append("NA")
+                turn["opp_amount"].append("NA")
+
+            else:
+                turn["your_action"].append(you.loc[:,"Action"].item())
+                turn["your_amount"].append(you.loc[:,"ActionAmt"].item())
+                turn["opp_action"].append("NA")
+                turn["opp_amount"].append("NA")
+
+        actions.append(turn)
+        
+    episode.add_turns(actions)
+
+    games.add_episode(episode)
+
+print(games)
