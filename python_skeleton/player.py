1f788115b389a3f580bcf50577f1a8f25907df3d
TBennur <tanay.bennur@gmail.com>
2024-03-29 23:53:56 -0400

    fixed bug

--- python_skeleton/player.py
+++ python_skeleton/player.py
@@ -9,7 +9,7 @@ import math
 import numpy as np
 from typing import Optional
 
-from arnav_prob import ArnavPlayer
+from antiallin_prob import ArnavPlayer
 from prob_bot import ProbPlayer
 from all_in import AllInPlayer
 
@@ -128,70 +128,8 @@ class Player(Bot):
         Returns:
             Action: The action you want to take.
         """
-<<<<<<< HEAD
         
         return self.bots[self.cur_bot].get_action(observation)
-=======
-        my_contribution = STARTING_STACK - observation["my_stack"] # the number of chips you have contributed to the pot
-        opp_contribution = STARTING_STACK - observation["opp_stack"] # the number of chips your opponent has contributed to the pot
-        pot_size = my_contribution + opp_contribution # the number of chips in the pot
-        continue_cost = observation["opp_pip"] - observation["my_pip"] # the number of chips needed to stay in the pot
-
-        self.log.append("My cards: " + str(observation["my_cards"]))
-        self.log.append("Board cards: " + str(observation["board_cards"]))
-        self.log.append("My stack: " + str(observation["my_stack"]))
-        self.log.append("My contribution: " + str(my_contribution))
-        self.log.append("My bankroll: " + str(observation["my_bankroll"]))
-
-        # Original probability calculation
-        # leftover_cards = [f"{rank}{suit}" for rank in "123456789" for suit in "shd" if f"{rank}{suit}" not in observation["my_cards"] + observation["board_cards"]]
-        # possible_card_comb = list(itertools.permutations(leftover_cards, 4 - len(observation["board_cards"])))
-        # possible_card_comb = [observation["board_cards"] + list(c) for c in possible_card_comb]
-        # result = map(lambda x: evaluate(observation["my_cards"], x[:2]) > evaluate(x[:2], x[2:]), possible_card_comb)
-        # prob = sum(result) / len(possible_card_comb)
-
-        # Use pre-computed probability calculation
-        equity = self.pre_computed_probs['_'.join(sorted(observation["my_cards"])) + '_' + '_'.join(sorted(observation["board_cards"]))]
-        pot_odds = continue_cost / (pot_size + continue_cost)
-
-        self.log.append(f"Equity: {equity}")
-        self.log.append(f"Pot odds: {pot_odds}")
-
-        # If the villain raised, adjust the probability
-        if continue_cost > 1:
-            if observation["opp_stack"] == 0:
-                self.num_shoves += 1
-        if (self.num_shoves / self.num_rounds >= 0.2 and 
-            (random.random() >= 0.1)):
-            if equity > 0.51 and (RaiseAction in observation["legal_actions"]):
-                action = RaiseAction(observation["max_raise"])
-            elif equity > 0.51 and (CallAction in observation["legal_actions"]):
-                action = CallAction()
-            elif CheckAction in observation["legal_actions"]:
-                action = CheckAction()
-            else:
-                action = FoldAction()
-        else:
-            if continue_cost > 1:
-                equity = (equity - 0.5) / 0.5
-                self.log.append(f"Adjusted equity: {equity}")
-            if equity > 0.9 and RaiseAction in observation["legal_actions"]:
-                action = RaiseAction(observation["max_raise"])
-            elif equity > 0.8 and RaiseAction in observation["legal_actions"]:
-                raise_amount = min(int(pot_size*0.75), observation["max_raise"])
-                raise_amount = max(raise_amount, observation["min_raise"])
-                action = RaiseAction(raise_amount)
-            elif CallAction in observation["legal_actions"] and equity >= pot_odds:
-                action = CallAction()
-            elif CheckAction in observation["legal_actions"]:
-                action = CheckAction()
-            else:
-                action = FoldAction()
-
-        self.log.append(str(action) + "\n")
-
-        return action
->>>>>>> dd8a972a1188ceb55b7e9306aac012f3fb657805
 
 if __name__ == '__main__':
     run_bot(Player(), parse_args())
